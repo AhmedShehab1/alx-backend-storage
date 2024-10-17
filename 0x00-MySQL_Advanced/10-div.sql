0847e09c8b462e2b77097a64a3a93eccc0c242ab
AhmedShehab1 <legendbtw2003@gmail.com>
2024-10-17 20:52:11 +0000

    Changed return type to FLOAT

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,7 +1,8 @@
 -- SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+DROP FUNCTION SafeDiv;
 DELIMITER //
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL
+RETURNS FLOAT
 DETERMINISTIC
 BEGIN
     IF b = 0 THEN
@@ -10,3 +11,4 @@ BEGIN
     RETURN a / b;
 END //
 DELIMITER ;
+
